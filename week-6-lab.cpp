a059ae5cf6c3e04f7b5040124f2239780d0974fe
ramankaur3 <43217332+ramankaur3@users.noreply.github.com>
2018-10-18 13:35:16 -0400

    my lab6

---
+++ week-6-lab.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    int y=100;
+    cout<<"Enter the value of y";
+    cin>>y;
+     if(y>=86)
+     cout<<"Grade A";
+     else if(y>=72)
+     cout<<"Grade B";
+     else if(y>=60)
+     cout<<"Grade C";
+     else if(y>=50)
+     cout<<"Grade D";
+     else
+     cout<<"Grade F";
+
+
+
+     
+}
